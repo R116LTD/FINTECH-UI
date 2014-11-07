623bb67b527bd47e025b149c895e0d6481c203b1
Pratik Patel <pratik.patel@aspiresoftware.in>
2014-11-07 16:20:13 +0530

    FN-13 : login page - update validation created directive for it

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -157,3 +157,35 @@ app.factory('Session', function(APPLICATION) {
   return Session; 
 });
 
+app.directive('showValidation', [function() {
+    return {
+        restrict: "A",
+        require:'form',
+        link: function(scope, element, attrs, formCtrl) {
+            element.find('.form-group').each(function() {
+                var $formGroup=$(this);
+                var $inputs = $formGroup.find('input[ng-model],textarea[ng-model],select[ng-model]');
+
+                if ($inputs.length > 0) {
+                    $inputs.each(function() {
+                        var $input=$(this);
+                        scope.$watch(function() {
+                            return $input.hasClass('ng-invalid');
+                        }, function(isInvalid) {
+                            var $spanEle = $formGroup.find('span');
+                            if ($spanEle.length > 0) {
+                              $spanEle.each(function() {
+                                  var $span=$(this);
+                                  $span.toggleClass('glyphicon-remove', isInvalid);
+                                  $span.toggleClass('glyphicon-ok', !isInvalid);    
+                              });
+                            }
+                            $formGroup.toggleClass('has-success', !isInvalid);
+                            $formGroup.toggleClass('has-error', isInvalid);
+                        });
+                    });
+                }
+            });
+        }
+    };
+}]);
